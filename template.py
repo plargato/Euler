2f54749ca526a3775c1d24b50dcad96eeda27e75
barak amiel <barak307@gmail.com>
2022-11-03 17:59:09 +0200

    add template for solutions with timer

---
+++ template.py
@@ -0,0 +1,10 @@
+from time import perf_counter
+
+start_time = perf_counter()
+
+## Your code here
+
+end_time = perf_counter()
+delta = end_time - start_time
+print(f'Solution: {0}')
+print(f'Time: {delta:0.4f}s')
